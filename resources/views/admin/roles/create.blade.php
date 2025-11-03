f554a578333143679705c4efe97f56ef4d6cdef0
Joseph Aguilar <joseph.aguilar@tecdesoftware.edu.mx>
2025-11-03 16:48:03 -0600

    feat(roles): add action buttons (edit,delete, create) to roles and table and fix breadcrumb links - Added edit and delete buttons in RolaTable and actions view -Create slor for "Nuevo" buttton in index view - Fixed breadcrumb links to user href instead of route

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -1,12 +1,13 @@
 <x-admin-layout title="Roles | Simify"
                 :breadcrumbs="[
     ['name' => 'Dashboard',
-    'route' => route('admin.dashboard')],
+    'href' => route('admin.dashboard')],
 
-    ['name' => 'Roles'],
-    ['name' => 'Create'],
+    ['name' => 'Roles',
+    'href' => route('admin.roles.index'),],
+    ['name' => 'Nuevo',],
 ]">
 
-    @livewire('admin.datatables.role-table')
+
 
 </x-admin-layout>
--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -1,12 +1,13 @@
 <x-admin-layout title="Roles | Simify"
                 :breadcrumbs="[
     ['name' => 'Dashboard',
-    'route' => route('admin.dashboard')],
+    'href' => route('admin.dashboard')],
 
-    ['name' => 'Roles'],
-    ['name' => 'Edit'],
+    ['name' => 'Roles',
+    'href' => route('admin.roles.index'),],
+    ['name' => 'Editar'],
 ]">
 
-    @livewire('admin.datatables.role-table')
+
 
 </x-admin-layout>
